6b63417abd698a26d3e8e44ebbed920d3b51f22c
simon <trns0286@humber.ca>
2023-04-21 00:25:31 -0400

    index fixing

--- scripts/index.js
+++ scripts/index.js
@@ -29,4 +29,15 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+
+  //automatic slideshow script
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
